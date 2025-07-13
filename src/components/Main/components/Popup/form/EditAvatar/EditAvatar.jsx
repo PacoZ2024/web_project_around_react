a72711c328db7aab7e941d354c426ae965a3b133
Megaman <pacosclub@gmail.com>
2025-07-13 14:04:35 -0600

    feat(EditAvatarButton): Se activa/desactiva el boton Editar Avatar deacuerdo con las entradas en el formulario

--- src/components/Main/components/Popup/form/EditAvatar/EditAvatar.jsx
+++ src/components/Main/components/Popup/form/EditAvatar/EditAvatar.jsx
@@ -1,13 +1,19 @@
-import { useRef, useContext } from "react";
+import { useState, useContext } from "react";
 import { CurrentUserContext } from "../../../../../../contexts/CurrentUserContext.js";
 
 export default function EditAvatar() {
+  const [isValid, setIsValid] = useState(false);
   const { handleUpdateAvatar } = useContext(CurrentUserContext);
-  const avatarImage = useRef();
+  const [avatarImage, setAvatarImage] = useState("");
+
+  const handleInputChange = (event) => {
+    setAvatarImage(event.target.value);
+    setIsValid(event.target.validity.valid);
+  };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    handleUpdateAvatar({ avatar: avatarImage.current.value });
+    handleUpdateAvatar({ avatar: avatarImage });
   };
 
   return (
@@ -15,15 +21,21 @@ export default function EditAvatar() {
       <fieldset className="form__content">
         <legend className="form__title">Cambiar foto de perfil</legend>
         <input
-          ref={avatarImage}
+          onChange={handleInputChange}
           id="link-image-profile"
           className="form__field form__field-link-image-profile"
           type="url"
           placeholder="Enlace a la imagen"
           required
+          pattern="https?://.+"
         />
         <span className="link-image-profile-error form__field-error"></span>
-        <button className="form__button" type="submit" onClick={handleSubmit}>
+        <button
+          className={`form__button ${!isValid ? "form__button-disabled" : ""}`}
+          type="submit"
+          onClick={handleSubmit}
+          disabled={!isValid}
+        >
           Guardar
         </button>
       </fieldset>
